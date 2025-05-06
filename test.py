93e1feff43f1d3f641be245b6c1f60aed628d74b
ZuliSerdar <serdar.zuli@gmail.com>
2025-05-06 11:11:01 +0200

    deleted unnecessary files

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-print(type(datetime.date(2012,1,1) - datetime.date(2010,1,1)))
-
-import itertools
-print([i for i in filter(lambda x: x%5, 
-                   itertools.islice(itertools.count(5),10))])
